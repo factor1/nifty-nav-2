bcb0aa98521c84ba936e0b487a1d177e90c586ee
Eric Stout <ericwstout@gmail.com>
2018-02-22 16:52:09 -0700

    initial commit of test files, project setup

---
+++ src/js/niftyNav2.js
@@ -0,0 +1,22 @@
+/**
+  * Nifty Nav 2
+  * A JavaScript menu framework.
+  * Author: Eric Stout, Factor1 Studios
+  * https://github.com/factor1/nifty-nav-2
+  */
+
+const init = () => {
+  console.log('Hello, World');
+}
+
+const burger = function() {
+  console.log('Burger Return!');
+}
+
+/*
+ * Export Public API
+ */
+module.exports = {
+  init,
+  burger
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/niftyNav2.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: path.resolve(__dirname, 'dist'),
+    filename: 'niftyNav2.js',
+    library: 'niftyNav2',
+    libraryTarget: 'umd',
+    sourceMapFilename: '[file].map'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+}
