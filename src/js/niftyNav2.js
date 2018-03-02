1f940fe50b09f198956ff87d8676c2b6e5d1bbf1
Eric Stout <ericwstout@gmail.com>
2018-03-01 17:40:18 -0700

    Add settings scss, menuText handling

--- src/js/niftyNav2.js
+++ src/js/niftyNav2.js
@@ -7,8 +7,6 @@
 
 import styles from '../scss/niftyNav2.scss';
 
-// Detect not supported browsers (<=IE9)
-const browserNotSupported = document.all && !window.atob;
 
 /**
  * Default Options
@@ -17,8 +15,8 @@ let options = {
   icon: 'square', // icon style (square, rounded)
   iconColor: '#fff', // default icon color
   iconColorActive: '#fff', // default active/open icon color
-  menuText: false, // toggle MENU text next to icon
-  menuString: 'Menu', // text to appear when menuText is true
+  showMenuText: false, // toggle text next to icon
+  menuText: 'Menu', // text to appear when showMenuText is true
   targets: ['niftyNav'], // targets can be an array so you can have multiple instances at a time <div data-nifty-target="niftyNav"></div>
   panelOrigin: 'top',  // where the panel will animate or originate from
   panelPosition: 'absolute', // css position - absolute, relative, fixed, etc...
@@ -27,16 +25,21 @@ let options = {
 }
 
 /**
- * Handle click of target listener
- */
+ *
+ * Handle click(s) of target listener
+ *
+**/
 const handleTargetClick = function(e) {
   e.target.classList.toggle('nifty-active');
 }
 
 /**
+ *
  * Build Icon Structure
- */
+ *
+**/
 const buildIcons = function(target, options) {
+
   const icon = `
     <button class="nifty-icon nifty-icon--${options.icon}">
       <span></span>
@@ -44,22 +47,24 @@ const buildIcons = function(target, options) {
   `;
   target.innerHTML = icon;
 
-  // if option for iconColor has been changed
-  if( options.iconColor !== '#fff' ) {
-    target.querySelector('span').style.backgroundColor = options.iconColor;
+  // if we are showing menu text
+  if( options.showMenuText === true ) {
+    target.querySelector('.nifty-icon').classList.add('nifty-icon--has-text');
+    target.querySelector('.nifty-icon').innerHTML = `<div class="nifty-icon--text">${options.menuText}</div><span></span>`;
   }
 
-  // if option for iconColorActive has been changed
-  if( options.iconColor !== '#fff' ) {
-    target.querySelector('span').style.backgroundColor = options.iconColor;
-  }
 }
 
 /**
+ *
  * Initialization of niftyNav2
- */
+ *
+**/
 const init = function(settings) {
   // quit if browser not supported - TODO: test this actually works.
+  // Detect not supported browsers (<=IE9)
+  const browserNotSupported = document.all && !window.atob;
+  
   if( browserNotSupported ) {
     return console.log('%c [Nifty Nav 2]: Browser not supported. Please upgrade your browser.', 'color: #rgb(232, 141, 57)');
   }
@@ -78,9 +83,11 @@ const init = function(settings) {
 
 }
 
-/*
+/**
+ *
  * Export Public API
- */
+ *
+**/
 module.exports = {
   init
 }
